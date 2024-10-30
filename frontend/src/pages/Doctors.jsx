9723de53afd564e1ae456cd3bf352dcd5c257114
Abhishek Buddiga <ad23b1012@iiitr.ac.in>
2024-10-31 02:27:28 +0530

    commit

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -5,6 +5,7 @@ import { AppContext } from '../context/AppContext';
 const Doctors = () => {
   const { speciality } = useParams();
   const [filterDoc, setFilterDoc] = useState([]);
+  const [showFilter, setShowFilter]=useState(false)
   const navigate = useNavigate();
 
   const { doctors } = useContext(AppContext);
@@ -25,6 +26,7 @@ const Doctors = () => {
     <div>
       <p className='text-gray-600'>Browse through the doctors specialist.</p>
       <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
+        <button onClick={()=> setShowFilter(prev => !prev)}>Filters</button>
         <div className='flex flex-col gap-4 text-sm text-gray-600'>
           <p onClick={()=> speciality === 'Gynecologist' ? navigate('/doctors') : navigate ('/doctors/Gynecologist')} className={`w-[94vw sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer]`}>Gynecologist</p>
           <p onClick={()=> speciality === 'General physician' ? navigate('/doctors') : navigate ('/doctors/General physician')} className={`w-[94vw sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer]`}>General physician</p>
